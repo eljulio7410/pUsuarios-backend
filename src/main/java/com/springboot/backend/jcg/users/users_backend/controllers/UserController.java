ba277ff3143ef19be389eb9c1a5f43ed9482f283
eljulio7410 <kaiser7420@gmail.com>
2024-07-31 16:34:07 -0500

    se agrega dependencia de validacion y se agregan validaciones en el entity y en el controller

--- src/main/java/com/springboot/backend/jcg/users/users_backend/controllers/UserController.java
+++ src/main/java/com/springboot/backend/jcg/users/users_backend/controllers/UserController.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.springboot.backend.jcg.users.users_backend.entities.User;
 import com.springboot.backend.jcg.users.users_backend.services.UserService;
 
+import jakarta.validation.Valid;
+
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +26,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -43,11 +49,19 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<User> create(@RequestBody User user) {
+    public ResponseEntity<?> create(@Valid @RequestBody User user, BindingResult result) {
+        if (result.hasErrors()) {
+            return validation(result);
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(user));
     }
     @PutMapping("/{id}")
-    public ResponseEntity<User> Update(@PathVariable Long id, @RequestBody User user){
+    public ResponseEntity<?> Update(@Valid @RequestBody User user, BindingResult result, @PathVariable Long id){
+
+        if (result.hasErrors()) {
+            return validation(result);
+        }
+
         Optional<User> userOptional = service.findById(id);
 
         if(userOptional.isPresent()) {
@@ -72,5 +86,12 @@ public class UserController {
         return ResponseEntity.notFound().build();
     }
     
+    private ResponseEntity<?> validation(BindingResult result) {
+        Map<String, String> errors = new HashMap<>();
+        result.getFieldErrors().forEach(error ->{
+            errors.put(error.getField(), "El campo" + error.getField() + " " + error.getDefaultMessage());
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
 
 }  
--- src/main/java/com/springboot/backend/jcg/users/users_backend/entities/User.java
+++ src/main/java/com/springboot/backend/jcg/users/users_backend/entities/User.java
@@ -5,6 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.*;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 
 @Entity
 @Table(name = "users")
@@ -15,14 +18,20 @@ public class User {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
+    @NotBlank
     private String name;
 
+    @NotBlank
     private String lastname;
 
+    @NotBlank
+    @Email
     private String email;
     
+    @NotBlank
     private String username;
 
+    @NotBlank
     private String password;
 
     public Long getId() {
